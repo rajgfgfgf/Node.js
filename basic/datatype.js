d7cacf5fe98e68bf2d50086123c1f60823c810cf
rajgfgfgf <pankajraj5756@gmail.com>
2024-11-27 05:18:17 +0000

    datatype

---
+++ basic/datatype.js
@@ -0,0 +1,14 @@
+"use strict" //newer version
+// permeative datatype
+// Number
+// String
+// Boolean = true/false
+// null
+// symbol = uniqe
+// undefined
+// BigInt
+let fun = "pankaj"
+let age = 23
+let bool = true
+
+console.table(typeof fun)
---
+++ basic/var.js
@@ -0,0 +1,15 @@
+console.log("PANKAJ");
+const studentId = 522
+let studentEmail = "hii@google.com"
+var studentPassword = "44444"
+state = "sikar"
+let user
+
+studentPassword = "33333"
+// studentId = "23423"
+
+
+// var are not use
+// becouse var is problum in functional and block
+
+console.table([studentId,studentEmail,studentPassword,state,user])
